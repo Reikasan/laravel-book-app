41a031cf50945846fac21ee995c6c01074cce0b6
Reika Akuzawa <l06981@googlemail.com>
2024-02-15 21:09:11 +0100

    Add favicon and update font link in app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,7 +9,10 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
+       
+        <!-- Favicon -->
+        <link rel=”shortcut icon” href="{{ asset('favicon.ico') }}" />
+       
         <!-- Script -->
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
