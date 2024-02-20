bced84dfe6c60ee40820f09cc36794f28a5c1918
Reika Akuzawa <l06981@googlemail.com>
2024-02-20 18:21:15 +0100

    Refactor BookController and add BookService

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,10 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
+use App\Services\BookService;
 
 class BookController extends Controller
 {
+    private $bookService;
+
+    public function __construct(BookService $bookService)
+    {
+        $this->bookService = $bookService;
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -65,35 +72,28 @@ class BookController extends Controller
         //
     }
 
-    public function showFetchedBooks(Request $request)
+    public function searchByApi(Request $request)
     {
         $bookTitle = $request->input('book-title');
-        $response = Http::get('https://www.googleapis.com/books/v1/volumes?q=' . $bookTitle);
-        $books = $response->json();
+        $books = $this->bookService->fetchBook('title', $bookTitle);
+        $books = $this->bookService->mapBooksFromApi($books);
 
-        $formattedBooks = [];
-        // dd($books['items']);
-        foreach($books['items'] as $book) {
-            $item = new Book();
-            $item->title = $book['volumeInfo']['title'];
-            $item->sub_title = isset($book['volumeInfo']['subtitle']) ? $book['volumeInfo']['subtitle'] : null;
-            $item->authors = isset($book['volumeInfo']['authors']) ? implode(', ', $book['volumeInfo']['authors']) : null;
-            $item->description = isset($book['volumeInfo']['description']) ? $book['volumeInfo']['description'] : null;
-            $item->image_link = isset($book['volumeInfo']['imageLinks']['thumbnail']) ? $book['volumeInfo']['imageLinks']['thumbnail'] : null;
-            $item->isbn =  isset($book['volumeInfo']['industryIdentifiers'][0]['identifier']) ? $book['volumeInfo']['industryIdentifiers'][0]['identifier'] : null;
-            $item->isbn13 = isset($book['volumeInfo']['industryIdentifiers'][1]['identifier']) ? $book['volumeInfo']['industryIdentifiers'][1]['identifier'] : null;
-            $item->language = isset($book['volumeInfo']['language']) ? $book['volumeInfo']['language'] : null;
-            $item->page_count = isset($book['volumeInfo']['pageCount']) ? $book['volumeInfo']['pageCount'] : null;
-            $item->publisher = isset($book['volumeInfo']['publisher']) ? $book['volumeInfo']['publisher'] : null;
-            $item->published_date = isset($book['volumeInfo']['publishedDate']) ? $book['volumeInfo']['publishedDate'] : null;
-            $item->category_id = isset($book['volumeInfo']['categories'])? $book['volumeInfo']['categories'][0] : null;
-            $item->google_book_id = $book['id'];
-
-            $formattedBooks[] = $item;
-        }
         return view('pages.books', [
-            'books' => $formattedBooks,
+            'books' => $books,
             'type' => 'fetch'
         ]);
+    }
+
+    
+
+    public function showFetchedBook(Request $request)
+    {
+
+        $isbn = $request->input('isbn') != null ? $request->input('isbn') : null; 
+
+        $book = $this->bookService->fetchBook('isbn', $isbn);
+        $book = $this->bookService->mapBooksFromApi($book);
+
+        return view('pages.book', ['book' => $book]);
     }
 }
---
+++ app/Services/BookService.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+use App\Models\Book;
+
+class BookService
+{
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Fetch book from Google Books API
+     */
+    public function fetchBook(string $type, string $keyword)
+    {
+        if($type === 'isbn') {
+            $response = Http::get('https://www.googleapis.com/books/v1/volumes?q=isbn:' . $keyword);
+        } else {
+            $response = Http::get('https://www.googleapis.com/books/v1/volumes?q=' . $keyword);
+        }
+        $books = $response->json();
+        return $books['items'];
+    }
+
+    public function mapBooksFromApi(array $books): array
+    {
+        $mappedBooks = [];
+        
+        foreach($books as $book) {
+            $item = new Book();
+            $item->title = $book['volumeInfo']['title'];
+            $item->sub_title = isset($book['volumeInfo']['subtitle']) ? $book['volumeInfo']['subtitle'] : null;
+            $item->authors = isset($book['volumeInfo']['authors']) ? implode(', ', $book['volumeInfo']['authors']) : null;
+            $item->description = isset($book['volumeInfo']['description']) ? $book['volumeInfo']['description'] : null;
+            $item->image_link = isset($book['volumeInfo']['imageLinks']['thumbnail']) ? $book['volumeInfo']['imageLinks']['thumbnail'] : null;
+            $item->isbn =  isset($book['volumeInfo']['industryIdentifiers'][0]['identifier']) ? $book['volumeInfo']['industryIdentifiers'][0]['identifier'] : null;
+            $item->isbn13 = isset($book['volumeInfo']['industryIdentifiers'][1]['identifier']) ? $book['volumeInfo']['industryIdentifiers'][1]['identifier'] : null;
+            $item->language = isset($book['volumeInfo']['language']) ? $book['volumeInfo']['language'] : null;
+            $item->page_count = isset($book['volumeInfo']['pageCount']) ? $book['volumeInfo']['pageCount'] : null;
+            $item->publisher = isset($book['volumeInfo']['publisher']) ? $book['volumeInfo']['publisher'] : null;
+            $item->published_date = isset($book['volumeInfo']['publishedDate']) ? $book['volumeInfo']['publishedDate'] : null;
+            $item->category_id = isset($book['volumeInfo']['categories'])? $book['volumeInfo']['categories'][0] : null;
+            $item->google_book_id = $book['id'];
+
+            $mappedBooks[] = $item;
+        }
+        return $mappedBooks;
+    }
+}
