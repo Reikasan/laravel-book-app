d3d739c785c9626e21e15870931dce5a1aa82cbd
Reika Akuzawa <l06981@googlemail.com>
2024-02-21 23:01:39 +0100

    Refactor book-card.blade.php layout

--- resources/views/components/book-card.blade.php
+++ resources/views/components/book-card.blade.php
@@ -1,23 +1,24 @@
-
 <div class="book-card">
+    <div class="book-card__img">
         <img src="{{ $book->image_thumbnail }}" alt="{{ $book->title }}"/>
-        <div class="book-card__content">
-            <div class="book-info">
-                <h3>{{ $book->title }}</h3>
-                <p>{{ $book->authors }}</p>
-            </div>
+    </div>
+    <div class="book-card__content">
+        <div class="book-info">
+            <h3>{{ $book->title }}</h3>
+            <p>{{ $book->authors }}</p>
         </div>
-        @if($type === 'fetch')
-        <form class="book-card__btn-container" method="POST" action={{ route('books.showFetchedBook', ['title' => $book->title])}}>
-            @csrf
-            <input type="hidden" name="isbn" value="{{ $book->isbn }}">
-            <input type="hidden" name="isbn13" value="{{ $book->isbn13 }}">
-            <button type="submit" class="btn btn-primary">Learn More</button>
-            <button class="btn btn-primary">Select</button>
-        </form>
-        @elseif($type === 'selected')
-        <div class="book-card__btn-container">
-            <button class="btn btn-primary" href="{{ route('books.show', ['book' => $book->id])}}">Check Book</button>
-        </div>
-        @endif  
+    </div>
+    @if($type === 'fetch')
+    <form class="book-card__btn-container" method="POST" action={{ route('books.showFetchedBook', ['title' => $book->title])}}>
+        @csrf
+        <input type="hidden" name="isbn" value="{{ $book->isbn }}">
+        <input type="hidden" name="isbn13" value="{{ $book->isbn13 }}">
+        <button type="submit" class="btn btn--secondary">Learn More</button>
+        <button class="btn btn--secondary">Select</button>
+    </form>
+    @elseif($type === 'selected')
+    <div class="book-card__btn-container">
+        <button class="btn btn--primary" href="{{ route('books.show', ['book' => $book->id])}}">Check Book</button>
+    </div>
+    @endif  
 </div>  
