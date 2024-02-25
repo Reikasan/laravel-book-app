8ebc8911f3384163859661fc7fef07346463dd53
Reika Akuzawa <l06981@googlemail.com>
2024-02-25 13:19:23 +0100

    Add BookRepository and modify BookService to handle book data

---
+++ app/Repositories/BookRepository.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\Repositories;
+
+use App\Models\Book;
+
+class BookRepository
+{
+    public function findBookByApiId(string $apiId): object | null 
+    {
+        return Book::where('google_book_id', $apiId)->first();
+    }
+
+    public function isBookInDatabase(string $apiId): bool
+    {
+        return Book::where('google_book_id', $apiId)->exists();
+    }
+
+    public function isBookReviewedByUser(int $bookId): bool
+    {
+        return Book::find($bookId)->reviews()->where('user_id', auth()->id())->exists();
+    }
+
+    public function getUserReview($book): object | null
+    {
+        $userId = auth()->id();
+        return $book->reviews->where('user_id', $userId)->first();
+    }
+}
--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -4,12 +4,15 @@ namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
 use App\Models\Book;
+use App\Repositories\BookRepository as BookRepository;
 
 class BookService
 {
-    public function __construct()
-    {
+    private $bookRepository;
 
+    public function __construct(BookRepository $bookRepository)
+    {
+        $this->bookRepository = $bookRepository;
     }
 
     /**
@@ -24,7 +27,7 @@ class BookService
             $response = Http::get('https://www.googleapis.com/books/v1/volumes?q=' . $keyword . '&maxResults=40');
         }
         $books = $response->json();
-        $books = $this->mapBooksFromApi($books['items']);
+        $books = $this->handleBookData($books['items']);
         return $type === 'isbn' ? $books[0] : $books;
     }
 
@@ -35,7 +38,7 @@ class BookService
         return str_replace(' ', '+', $keyword);
     }
 
-    public function mapBooksFromApi(array $books): array
+    public function handleBookData(array $books): array
     {
         $mappedBooks = [];
         
@@ -44,25 +47,38 @@ class BookService
                 continue;
             }
 
-            $item = new Book();
-            $item->title = $book['volumeInfo']['title'];
-            $item->sub_title = isset($book['volumeInfo']['subtitle']) ? $book['volumeInfo']['subtitle'] : null;
-            $item->authors = implode(', ', $book['volumeInfo']['authors']);
-            $item->description = isset($book['volumeInfo']['description']) ? $book['volumeInfo']['description'] : null;
-            $item->image_thumbnail = isset($book['volumeInfo']['imageLinks']['thumbnail']) ? $book['volumeInfo']['imageLinks']['thumbnail'] : null;
-            $item->image_large = isset($book['volumeInfo']['imageLinks']['medium']) ? $book['volumeInfo']['imageLinks']['medium'] : null;
-            $item->isbn =  isset($book['volumeInfo']['industryIdentifiers'][0]['identifier']) ? $book['volumeInfo']['industryIdentifiers'][0]['identifier'] : null;
-            $item->isbn13 = isset($book['volumeInfo']['industryIdentifiers'][1]['identifier']) ? $book['volumeInfo']['industryIdentifiers'][1]['identifier'] : null;
-            $item->language = isset($book['volumeInfo']['language']) ? $book['volumeInfo']['language'] : null;
-            $item->page_count = isset($book['volumeInfo']['pageCount']) ? $book['volumeInfo']['pageCount'] : null;
-            $item->publisher = isset($book['volumeInfo']['publisher']) ? $book['volumeInfo']['publisher'] : null;
-            $item->published_date = isset($book['volumeInfo']['publishedDate']) ? $book['volumeInfo']['publishedDate'] : null;
-            $item->categories = isset($book['volumeInfo']['categories'])? $book['volumeInfo']['categories'][0] : null;
-            $item->google_book_id = $book['id'];
-            $item->google_book_link = $book['selfLink'];
+            if($this->bookRepository->isBookInDatabase($book['id'])) {
+                $item = $this->bookRepository->findBookByApiId($book['id']);
+                $item->isBookInDatabase = true;
+                $item->isReviewedByUser = $this->bookRepository->isBookReviewedByUser($item->id);
+            } else {
+                $item = new Book();
+                $item->title = $book['volumeInfo']['title'];
+                $item->sub_title = isset($book['volumeInfo']['subtitle']) ? $book['volumeInfo']['subtitle'] : null;
+                $item->authors = implode(', ', $book['volumeInfo']['authors']);
+                $item->description = isset($book['volumeInfo']['description']) ? $book['volumeInfo']['description'] : null;
+                $item->image_thumbnail = isset($book['volumeInfo']['imageLinks']['thumbnail']) ? $book['volumeInfo']['imageLinks']['thumbnail'] : null;
+                $item->image_large = isset($book['volumeInfo']['imageLinks']['medium']) ? $book['volumeInfo']['imageLinks']['medium'] : null;
+                $item->isbn =  isset($book['volumeInfo']['industryIdentifiers'][0]['identifier']) ? $book['volumeInfo']['industryIdentifiers'][0]['identifier'] : null;
+                $item->isbn13 = isset($book['volumeInfo']['industryIdentifiers'][1]['identifier']) ? $book['volumeInfo']['industryIdentifiers'][1]['identifier'] : null;
+                $item->language = isset($book['volumeInfo']['language']) ? $book['volumeInfo']['language'] : null;
+                $item->page_count = isset($book['volumeInfo']['pageCount']) ? $book['volumeInfo']['pageCount'] : null;
+                $item->publisher = isset($book['volumeInfo']['publisher']) ? $book['volumeInfo']['publisher'] : null;
+                $item->published_date = isset($book['volumeInfo']['publishedDate']) ? $book['volumeInfo']['publishedDate'] : null;
+                $item->categories = isset($book['volumeInfo']['categories'])? $book['volumeInfo']['categories'][0] : null;
+                $item->google_book_id = $book['id'];
+                $item->google_book_link = $book['selfLink'];
+                $item->isBookInDatabase = false;
+                $item->isReviewedByUser = false;
+            }
 
             $mappedBooks[] = $item;
         }
         return $mappedBooks;
+    }
+
+    public function getUserReview(Book $book)
+    {
+        return $this->bookRepository->getUserReview($book);
     }
 }
