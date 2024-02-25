7a5282a1113514639e4727fac0f010de2db15bb8
Reika Akuzawa <l06981@googlemail.com>
2024-02-25 13:23:37 +0100

    Remove unused code in Book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -4,20 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Facades\App;
 
 class Book extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
-
-    public function category(): BelongsTo
-    {
-        return $this->belongsTo(Category::class);
-    }
 
     public function reviews(): HasMany
     {
