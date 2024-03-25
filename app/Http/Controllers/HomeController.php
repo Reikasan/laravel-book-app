ce54b3c0df86f4544eeef798d4006930daed14d7
Reika Akuzawa <l06981@googlemail.com>
2024-03-25 14:07:46 +0100

    Add WishlistService to HomeController constructor and retrieve wishlist in index method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,21 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Services\ReviewService;
+use App\Services\WishlistService;
 
 class HomeController extends Controller
 {
     private $reviewService;
+    private $wishlistService;
 
-    public function __construct(ReviewService $reviewService)
+    public function __construct(ReviewService $reviewService, WishlistService $wishlistService)
     {
         $this->reviewService = $reviewService;
-    
+        $this->wishlistService = $wishlistService;
     }
 
     public function index()
     {
         $userId = auth()->user()->id;
         $latestReviews = $this->reviewService->getLatestReviews($userId);
-        return view('pages.index', ['latestReviews' => $latestReviews]);
+        $wishlist = $this->wishlistService->getAll();
+        return view('pages.index', [
+            'latestReviews' => $latestReviews,
+            'wishlist' => $wishlist,
+        ]);
     }
 }
