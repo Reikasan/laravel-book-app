2f1e244695a64b3f1abc3e498fd8acacf785610e
Reika Akuzawa <l06981@googlemail.com>
2024-03-28 11:40:45 +0100

    Add menu button component and update review page

--- resources/views/pages/review.blade.php
+++ resources/views/pages/review.blade.php
@@ -1,13 +1,41 @@
 <x-app>
     <div class="review">
+        <div class="menu-btn">
+            <i class="fa-solid fa-ellipsis-vertical"></i>
+            <ul class="menu hidden">
+                <li class="menu__item">
+                    <a href="{{ route('reviews.edit', $review->id) }}" class="">
+                        <i class="fa-solid fa-pen"></i>
+                        Edit
+                    </a>
+                </li>
+                <li class="menu__item">
+                    <form action="{{ route('reviews.destroy', $review->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <div type="submit" class="">
+                            <i class="fa-solid fa-eraser"></i>
+                            Delete
+                        </div>
+                    </form>
+                </li>
+                <li class="menu__item">
+                    <a href="{{ route('books.show', $review->book->id) }}" class="">
+                        <i class="fa-solid fa-book"></i>
+                        Book details
+                    </a>
+                </li>
+            </ul>
+        </div>
         <div class="book-details">
             <div class="book-details__img">
-                <a href="{{ route('books.show', $review->book->id) }}" class="book-image-link">
                     <img 
                         src="{{ isset($review->book->image_large) ? $review->book->image_large : $review->book->image_thumbnail }}" 
                         alt="{{ $review->book->title }}"/>
-                    <div class="overlay">
-                        <p>Details</p>
+                    <div class="img-overlay">
+                        <a href="{{ route('books.show', $review->book->id) }}" class="btn">
+                            Details
+                        </a>
                     </div>
                 </a>
             </div> 
@@ -34,9 +62,9 @@
             <h2>Review</h2>
             <p>{{ $review->review }}</p>
         </div>
-        <div class="btn-container">
+        {{-- <div class="btn-container">
             <a href="{{ route('reviews.edit', $review->id) }}" class="btn btn--primary">Edit review</a>
             <a href="{{route('reviews.index')}}" class="btn btn--secondary">Check more reviews</a>
-        </div>
+        </div> --}}
     </div>
 </x-app>
